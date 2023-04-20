ca5b21ad06e8558ee36d26d63d86c51c27f54e14
Naan Mohammed <naanahmed24@gmail.com>
2023-04-20 23:04:56 +0300

    Add submit score function

---
+++ src/modules/submitScore.js
@@ -0,0 +1,28 @@
+const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
+const gameID = 'yk8C3WexuihEBieTHONU';
+
+export const submitScore = async (event) => {
+  event.preventDefault();
+  const user = document.getElementById('nameInput').value;
+  const score = document.getElementById('scoreInput').value;
+
+  if (!user || !score) {
+    alert('Please enter both name and score');
+    return;
+  }
+
+  const data = { user, score };
+
+  await fetch(`${baseURL}/games/${gameID}/scores/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+
+  document.getElementById('nameInput').value = '';
+  document.getElementById('scoreInput').value = '';
+};
+
+export default submitScore;
